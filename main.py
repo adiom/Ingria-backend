76afced14af12c04f3f11a289be1aaf97664c56f
Adiom Timur <adiom@canfly.org>
2025-01-16 06:54:43 +0400

    Update 4 vercel

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ app = FastAPI(title="Ingria Media Analyzer API")
 # Настройка CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://ingria.canfly.org"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
